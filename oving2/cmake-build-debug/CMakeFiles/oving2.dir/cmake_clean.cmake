4e9592cd1a5c04c1f2e31f032f3abd0b744df353
Hasan Rehman Omarzae <hasanro@stud.ntnu.no>
2022-08-22 19:08:47 +0200

    first commit

---
+++ oving2/cmake-build-debug/CMakeFiles/oving2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oving2.dir/main.cpp.obj"
+  "CMakeFiles/oving2.dir/main.cpp.obj.d"
+  "liboving2.dll.a"
+  "oving2.exe"
+  "oving2.exe.manifest"
+  "oving2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oving2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
